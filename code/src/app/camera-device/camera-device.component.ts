12c3d025c54ac5910250deefa53ac52920017b73
Arnaud <arnaud.nilens@gmail.com>
2020-02-21 22:58:55 +0100

    Camera update

--- code/src/app/camera-device/camera-device.component.ts
+++ code/src/app/camera-device/camera-device.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 
+// @ts-ignore
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-camera-device',
+  templateUrl: './camera-device.component.html',
+  styleUrls: ['./camera-device.component.css']
 })
-export class AppComponent {
+export class CameraDeviceComponent implements OnInit {
+
   @ViewChild('videoElement') videoElement: any;
   video: any;
+
+  constructor() { }
 
   ngOnInit() {
     this.video = this.videoElement.nativeElement;
   }
+
+
 }
