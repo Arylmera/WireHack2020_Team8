90bf8916163e7950bd654571f1eeca453db18189
guillaumelemer <guillaume@lemer.be>
2020-02-21 22:59:58 +0100

    intro map

--- code/src/app/components/form-login/form-login.component.ts
+++ code/src/app/components/form-login/form-login.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.css']
 })
+
 export class FormLoginComponent implements OnInit {
 
   constructor() { }
