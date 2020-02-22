39c37b54998e909d8b5a5a1b40d6170dc4e14ba6
guillaumelemer <guillaume@lemer.be>
2020-02-22 08:03:51 +0100

    push map and tchat

--- code/src/app/components/tchat-zombie/tchat-zombie.component.ts
+++ code/src/app/components/tchat-zombie/tchat-zombie.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TchatZombieComponent implements OnInit {
 
+  value = 'clear me';
   messageList: string[] = [''];
 
   constructor() { }
