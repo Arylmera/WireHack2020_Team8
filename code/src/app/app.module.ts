aa039a714f87c6484d7380f349e71bddb776dddd
guillaumelemer <guillaume@lemer.be>
2020-02-22 03:15:36 +0100

    pause

--- code/src/app/app.module.ts
+++ code/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
@@ -11,19 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-<<<<<<< HEAD
-import {MatButtonModule} from "@angular/material/button";
+import {MatButtonModule} from '@angular/material/button';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { LayoutModule } from '@angular/cdk/layout';
-
-=======
-import {MatInputModule} from '@angular/material/input';
 import {Route} from '@angular/router';
->>>>>>> 45fbaf7559efa1f4f484a5f59669e5601f469699
 
 const appRoutes: Route = [
   {path: 'login', component: LoginComponent}
